3df3a6aa1951f3aaaf98be0aa43f8c781548097a
Sebas <sebasverac331@gmail.com>
2025-02-11 16:00:41 -0600

    style(all-courses): Me muestra un pequeño badge en el botón de administrar las solicitudes de apertura indicando que hay solicitudes pendientes

--- Frontend/src/app/pages/management/dashboard/dashboard.component.ts
+++ Frontend/src/app/pages/management/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
+import { CoursesService } from '../../../core/services/courses.service';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
@@ -7,6 +8,9 @@ import { MatListModule } from '@angular/material/list';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { CourseFullData } from '../../../core/services/courses.service';
+import { MatBadgeModule } from '@angular/material/badge';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -20,18 +24,37 @@ import { MatExpansionModule } from '@angular/material/expansion';
     MatToolbarModule,
     MatExpansionModule,
     RouterLink,
+    MatBadgeModule,
   ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
 })
-export class ManagementDashboardComponent {
+export class ManagementDashboardComponent implements OnInit {
   username: string | null = '';
+  pendingCoursesCount = 0;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private coursesService: CoursesService) {}
 
   ngOnInit(): void {
-    // Puedes obtener los datos del administrador, por ejemplo, del localStorage
     this.username = localStorage.getItem('username');
+    this.loadPendingCoursesCount();
+  }
+
+  loadPendingCoursesCount(): void {
+    this.coursesService.getAllCourses().subscribe({
+      next: (courses: CourseFullData[]) => {
+        // Filtrar los cursos que tienen status = 'submitted' y approvalStatus != 'approved'
+        const filteredCourses = courses.filter(
+          (course) =>
+            course.status === 'submitted' &&
+            course.approvalStatus !== 'approved'
+        );
+        this.pendingCoursesCount = filteredCourses.length;
+      },
+      error: (err) => {
+        console.error('Error al obtener cursos:', err);
+      },
+    });
   }
 
   navigateTo(route: string): void {
